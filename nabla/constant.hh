06dd0310613f891ad09424971008bc128d808b96
Florian Knauf <f.knauf@mmkf.de>
2016-03-16 23:36:38 +0100

    constant folding optimization: unary op-

--- nabla/constant.hh
+++ nabla/constant.hh
@@ -40,6 +40,8 @@ namespace nabla {
     inline constant operator*(constant const &lhs, constant const &rhs) { return lhs.value() * rhs.value(); }
     inline constant operator/(constant const &lhs, constant const &rhs) { return lhs.value() / rhs.value(); }
 
+    inline constant operator-(constant const &x) { return -x.value(); }
+
     inline constant pow(constant const &base, constant &exponent) { return std::pow(base.value(), exponent.value()); }
     inline constant log(constant const &x) { return std::log(x.value()); }
   }
